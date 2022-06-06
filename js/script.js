16759b17961e67d607094dcea749771e20046713
sudevdhali <60395148+sudevdhali@users.noreply.github.com>
2022-06-07 03:27:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,15 +13,15 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
+var homeHtmlUrl = "snippets/home-snippet.php";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
-var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-var categoryHtml = "snippets/category-snippet.html";
+var categoriesTitleHtml = "snippets/categories-title-snippet.php";
+var categoryHtml = "snippets/category-snippet.php";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "snippets/menu-item.html";
+var menuItemsTitleHtml = "snippets/menu-items-title.php";
+var menuItemHtml = "snippets/menu-item.php";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
